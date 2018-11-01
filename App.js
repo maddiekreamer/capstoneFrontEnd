0d9525ec66a756168c54d4e87897b13c5e3e3889
Maddie Kreamer <maddiekreamer@gmail.com>
2018-11-01 15:42:54 -0600

    header/footer components successfully added, working on three card spread component being added

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
+// import ThreeCard from './components/ThreeCard';
 import Footer from './components/Footer';
-import ThreeCard from './components/ThreeCardSpread';
 
-export default class HelloWorldApp extends Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Header headerText="Tarot Readings"/>
-        <ThreeCard threeText="testing"/>
+        {/* <ThreeCard threeText="testing"/> */}
         <Footer footerText="Copyright 2018"/>
       </View>
     );
