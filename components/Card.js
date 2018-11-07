54945421804cde6a52096bebc605ad019a856376
Maddie Kreamer <maddiekreamer@gmail.com>
2018-11-06 22:10:59 -0700

    unnecessary code deleted, semantics/consistency achieved

--- components/Card.js
+++ components/Card.js
@@ -1,22 +1,8 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from "react"
+import { View, Text } from "react-native"
 
 const Card = props => {
-  return <View style={styles.containerStyle}>{props.children}</View>;
-};
-
-const styles = {
-  containerStyle: {
-    borderWidth: 1,
-    borderRadius: 2,
-    borderColor: "#ddd",
-    borderBottomWidth: 0,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 2,
-    elevation: 1
-  }
-};
+  return <View>{props.children}</View>
+}
 
 export default Card;
